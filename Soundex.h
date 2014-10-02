7a7a1735fc1470a6bc4c9341d362de3b0effac4a
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-02 13:30:46 +0200

    Chapter 2 / 14

--- Soundex.h
+++ Soundex.h
@@ -1,9 +1,9 @@
 /***
  * Excerpted from "Modern C++ Programming with Test-Driven Development",
  * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material,
+ * Copyrights apply to this code. It may not be used to create training material, 
  * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose.
+ * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/lotdd for more book information.
 ***/
 #ifndef Soundex_h
@@ -16,7 +16,6 @@ class Soundex
 public:
    std::string encode(const std::string& word) const {
       auto encoded = word.substr(0, 1);
-
       if (word.length() > 1)
          encoded += "1";
       return zeroPad(encoded);
@@ -24,7 +23,8 @@ public:
 
 private:
    std::string zeroPad(const std::string& word) const {
-      return word + "000";
+      auto zerosNeeded = 4 - word.length();
+      return word + std::string(zerosNeeded, '0');
    }
 };
 
