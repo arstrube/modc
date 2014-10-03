62ac34202e4ef0ecbdae699ca38109bf337d943a
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-03 21:15:36 +0200

    Chapter 2 / 27

--- Soundex.h
+++ Soundex.h
@@ -32,12 +32,15 @@ private:
 
    std::string encodedDigits(const std::string& word) const {
       std::string encoding;
-      for (auto letter: word) 
-      {
-         if (encoding.length() == MaxCodeLength - 1) break; 
+      for (auto letter: word) {
+         if (isComplete(encoding)) break;
          encoding += encodedDigit(letter);
       }
       return encoding;
+   }
+
+   bool isComplete (const std::string& encoding) const {
+      return encoding.length() == MaxCodeLength - 1; 
    }
 
    std::string encodedDigit(char letter) const {
