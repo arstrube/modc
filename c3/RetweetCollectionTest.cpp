7730ec6b16d9630e2e4764d8f698d831087c8957
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-21 17:30:36 +0200

    Chapter 3 / 9

--- c3/RetweetCollectionTest.cpp
+++ c3/RetweetCollectionTest.cpp
@@ -7,6 +7,17 @@ TEST_GROUP(ARetweetCollection) {
    RetweetCollection collection;
 };
 
+/** CppUTest has no equivalent to MATCHER_P()
+ *  Use a macro instead. Why a macro?
+ *  A macro will fail _in_ the test case, wheras
+ *  a helper method will fail in the helper
+ *  method, making debugging more difficult.
+ */
+#define HAS_SIZE(arg, expected) { \
+   LONGS_EQUAL(expected, arg.size()); \
+   CHECK(arg.isEmpty() == (0 == expected)); \
+}
+
 TEST(ARetweetCollection, IsEmptyWhenCreated) {
    CHECK(collection.isEmpty());
 }
@@ -25,15 +36,8 @@ TEST(ARetweetCollection, HasSizeOfOneAfterTweetAdded) {
    LONGS_EQUAL(1, collection.size());
 }
 
-TEST(ARetweetCollection, DecreasesSizeAfterRemovingTweet) {
-   collection.add(Tweet());
-   collection.remove(Tweet());
-   LONGS_EQUAL(0, collection.size());
-}
-
-// AVOID doing this
 TEST(ARetweetCollection, IsEmptyAfterRemovingTweet) {
    collection.add(Tweet());
    collection.remove(Tweet());
-   CHECK(collection.isEmpty());
+   HAS_SIZE(collection, 0);
 }
