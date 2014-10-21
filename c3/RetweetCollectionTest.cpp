980365d19b77694437874da5f597e100b55b0075
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-21 16:42:16 +0200

    Chapter 3 / 1 : Make it compile.

--- c3/RetweetCollectionTest.cpp
+++ c3/RetweetCollectionTest.cpp
@@ -1,8 +1,12 @@
 #include "cpputest/testharness.h"
 #include "cpputestext/mocksupport.h"
 
+class RetweetCollection {};
+
 TEST_GROUP(ARetweetCollection) {};
 
 TEST(ARetweetCollection, IsEmptyWhenCreated) {
    RetweetCollection retweets;
+   (void)retweets;
+   FAIL("To be implemented");
 }
