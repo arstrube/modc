75bc022a4df94c9d2f390e4ce3b687d827ef6360
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-21 14:59:01 +0200

    Chapter 2 / 40 : Add CharUtil.

---
+++ CharUtilTest.cpp
@@ -0,0 +1,56 @@
+#include "cpputest/testharness.h"
+#include "cpputestext/mocksupport.h"
+
+#include <string>
+#include "CharUtil.h"
+
+using namespace std;
+using namespace charutil;
+
+TEST_GROUP(AChar) {};
+
+TEST(AChar, IsAVowelForSixSpecificLetters) {
+   CHECK(isVowel('A'));
+   CHECK(isVowel('E'));
+   CHECK(isVowel('I'));
+   CHECK(isVowel('O'));
+   CHECK(isVowel('U'));
+   CHECK(isVowel('Y'));
+}
+
+TEST(AChar, IsAVowelForLowercaseLetters) {
+   CHECK(isVowel('a'));
+   CHECK(isVowel('e'));
+   CHECK(isVowel('i'));
+   CHECK(isVowel('o'));
+   CHECK(isVowel('u'));
+   CHECK(isVowel('y'));
+}
+
+TEST(AChar, IsNotAVowelForAnyOtherCharacter) {
+   CHECK_FALSE(isVowel('b'));
+}
+
+TEST(AChar, AnswersAppropriateUpperCaseLetter) {
+   CHECK(upper('a') == 'A');
+}
+
+TEST(AChar, HandlesAlreadyUppercasedLetters) {
+   CHECK(upper('B') == 'B');
+}
+
+TEST(AChar, IgnoresNonLettersWhenUppercasing) {
+  CHECK(upper('+') == '+');
+}
+
+TEST(AChar, AnswersAppropriateLowerCaseLetter) {
+   CHECK(lower('A') == 'a');
+}
+
+TEST(AChar, HandlesAlreadyLowercased) {
+   CHECK(lower('b') == 'b');
+}
+
+TEST(AChar, IgnoresNonLettersWhenLowercasing) {
+   CHECK(lower('+') == '+');
+}
