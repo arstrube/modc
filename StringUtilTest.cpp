1fe7644abd6822a7c4ad3a7c37f00ce94351d369
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-21 15:03:06 +0200

    Chapter 2 / 40 : Add StringUtil.

---
+++ StringUtilTest.cpp
@@ -0,0 +1,30 @@
+#include "cpputest/testharness.h"
+#include "cpputestext/mocksupport.h"
+
+#include "StringUtil.h"
+
+using namespace std;
+using namespace stringutil;
+
+TEST_GROUP(AString) {};
+
+TEST(AString, AnswersHeadAsItsFirstLetter) {
+   STRCMP_EQUAL("x", head("xyz").c_str());
+}
+
+TEST(AString, AnswersHeadAsEmptyWhenEmpty) {
+   STRCMP_EQUAL("", head("").c_str());
+}
+
+TEST(AString, AnswersTailAsAllLettersAfterHead) {
+   STRCMP_EQUAL("yz", tail("xyz").c_str());
+}
+
+TEST(AString, AnswersTailAsEmptyWhenEmpty) {
+   STRCMP_EQUAL("", tail("").c_str());
+}
+
+TEST(AString, AnswersTailAsEmptyWhenContainsOnlyOneCharacter) {
+   STRCMP_EQUAL("", tail("X").c_str());
+}
+
