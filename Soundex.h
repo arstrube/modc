9e68462a758064e3a25b2e6499190424580ffdfb
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-21 13:50:25 +0200

    Sync Soundex.h with book code.

--- Soundex.h
+++ Soundex.h
@@ -21,19 +21,6 @@ public:
       return zeroPad(upperFront(head(word)) + encodedDigits(tail(word)));
    }
 
-   std::string encodedDigit(char letter) const {
-      const std::unordered_map<char, std::string> encodings {
-         {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
-         {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
-         {'d', "3"}, {'t', "3"},
-         {'l', "4"},
-         {'m', "5"}, {'n', "5"},
-         {'r', "6"}
-      };
-      auto it = encodings.find(letter);
-      return it == encodings.end() ? "" : it->second;
-   }
-
 private:
    std::string head(const std::string& word) const {
       return word.substr(0, 1);
@@ -53,22 +40,39 @@ private:
       return encoding;
    }
 
-   bool isComplete (const std::string& encoding) const {
-      return encoding.length() == MaxCodeLength - 1; 
-   }
-
-   std::string zeroPad(const std::string& word) const {
-      auto zerosNeeded = MaxCodeLength - word.length();
-      return word + std::string(zerosNeeded, '0');
-   }
-   
    std::string lastDigit(const std::string& encoding) const {
       if (encoding.empty()) return "";
       return std::string(1, encoding.back());
    }
-   
+   // ...
+
+   bool isComplete (const std::string& encoding) const {
+      return encoding.length() == MaxCodeLength - 1; 
+   }
+
+public:
+   std::string encodedDigit(char letter) const {
+      const std::unordered_map<char, std::string> encodings {
+         {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+         {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
+         {'d', "3"}, {'t', "3"},
+         {'l', "4"},
+         {'m', "5"}, {'n', "5"},
+         {'r', "6"}
+      };
+      auto it = encodings.find(letter);
+      return it == encodings.end() ? "" : it->second;
+   }
+
+private:
+   std::string zeroPad(const std::string& word) const {
+      auto zerosNeeded = MaxCodeLength - word.length();
+      return word + std::string(zerosNeeded, '0');
+   }
+
    std::string upperFront(const std::string& string) const {
-      return std::string(1, std::toupper(static_cast<unsigned char>(string.front())));
+      return std::string(1, 
+            std::toupper(static_cast<unsigned char>(string.front())));
    }
 };
 
