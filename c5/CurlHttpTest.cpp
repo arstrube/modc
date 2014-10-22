365c08787baf152c4bd8f5ec7384d1ba8b715a7d
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-22 16:11:27 +0200

    Chapter 5 / 1 : Sanitize CurlHttpTest.cpp
    for CppUTest.

--- c5/CurlHttpTest.cpp
+++ c5/CurlHttpTest.cpp
@@ -1,22 +1,26 @@
-#include "gmock/gmock.h"
+#include "cpputest/testharness.h"
+#include "cpputestext/mocksupport.h"
 #include <string>
 #include "CurlHttp.h"
 
-using namespace ::testing;
 using namespace std;
+
+TEST_GROUP(Http) {};
 
 TEST(Http, WriteCallback) {
    string buffer("123456789012");
    size_t size(12);
    size_t numberOfMembers(1);
    CurlHttp::writeCallback("123456789012", size, numberOfMembers, NULL);
-   ASSERT_THAT(CurlHttp::Response(), Eq("123456789012"));
+   STRCMP_EQUAL("123456789012", CurlHttp::Response().c_str());
 }
+
+TEST_GROUP(HttpIntegration) {};
 
 TEST(HttpIntegration, Get) {
    CurlHttp http;
    http.initialize();
    auto response = http.get("http://langrsoft.com");
-   ASSERT_THAT(response, HasSubstr("Jeff Langr"));
+   STRCMP_CONTAINS("Jeff Langr", response.c_str());
 }
 
