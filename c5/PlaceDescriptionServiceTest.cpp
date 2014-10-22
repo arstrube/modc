c7c23a70aa77783de35672dd7f5ea25d387cbb41
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-22 15:24:22 +0200

    Chapter 5 / 1 : Sanitize PlaceDescriptionServiceTest.cpp
    for CppUTest.

--- c5/PlaceDescriptionServiceTest.cpp
+++ c5/PlaceDescriptionServiceTest.cpp
@@ -1,32 +1,38 @@
-#include "gmock/gmock.h"
+#include "cpputest/testharness.h"
+#include "cpputestext/mocksupport.h"
 
 #include "PlaceDescriptionService.h"
 #include "Http.h"
 
 using namespace std;
-using namespace testing;
 
-class APlaceDescriptionService: public Test {
+class PlaceDescriptionServiceFixture {
 public:
    static const string ValidLatitude;
    static const string ValidLongitude;
 };
 
-const string APlaceDescriptionService::ValidLatitude("38.005");
-const string APlaceDescriptionService::ValidLongitude("-104.44");
+const string PlaceDescriptionServiceFixture::ValidLatitude("38.005");
+const string PlaceDescriptionServiceFixture::ValidLongitude("-104.44");
+
+TEST_GROUP(APlaceDescriptionService) {
+   PlaceDescriptionServiceFixture f;
+};
 
 class HttpStub: public Http {
    void initialize() override {}
    std::string get(const std::string& url) const override {
+      (void) url;
       return "???";
    }
 };
 
-TEST_F(APlaceDescriptionService, ReturnsDescriptionForValidLocation) {
+TEST(APlaceDescriptionService, ReturnsDescriptionForValidLocation) {
    HttpStub httpStub;
    PlaceDescriptionService service{&httpStub};
 
-   auto description = service.summaryDescription(ValidLatitude, ValidLongitude);
+   auto description = service.summaryDescription(f.ValidLatitude, f.ValidLongitude);
 
-   ASSERT_THAT(description, Eq("Drury Ln, Fountain, CO, US"));
+   STRCMP_EQUAL("Drury Ln, Fountain, CO, US", description.c_str());
 }
+
