a652fb059262071d4a5fd3c40dc2dde0b362e1b8
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-30 17:37:28 +0100

    Chapter 7 / 1 - minimal boost.

---
+++ c7/boost/date_time/gregorian/gregorian_types.hpp
@@ -0,0 +1,54 @@
+#ifndef DATE_TIME_DATE_HPP___
+#define DATE_TIME_DATE_HPP___
+
+namespace boost {
+namespace gregorian {
+
+/** Header stub for class boost::gregorian::date */
+
+#define ARBITRARY_DATE date(2014, Mar, 15)
+
+enum Month {Jan=1,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec};
+
+class date_duration {
+public:
+   explicit date_duration(int day_count = 0) : d_(day_count) {}
+   unsigned get_days() const { return d_; }
+private:
+   unsigned d_;
+};
+
+class date {
+public:
+   date(unsigned y, Month m, unsigned d) : y_(y), m_(m), d_(d) {}
+   date() {
+      d_ = -1;
+   }
+   bool operator==(const date& rhs) const {
+      return d_ == rhs.d_ && m_ == rhs.m_ && y_ == rhs.y_;
+   }
+   date operator+(const date& rhs) const {
+      return date(y_, m_, d_ + rhs.d_);
+   }
+   date operator+(const date_duration& rhs) const {
+      return date(y_, m_, d_ + rhs.get_days());
+   }
+   bool is_not_a_date() {
+       return d_ < 0;
+   }
+private:
+   unsigned y_;
+   Month m_;
+   int d_;
+};
+
+class day_clock {
+public:
+  static date local_day() { return ARBITRARY_DATE; };
+  static date universal_day() { return ARBITRARY_DATE; }
+};
+
+
+} } // namespaces
+
+#endif
