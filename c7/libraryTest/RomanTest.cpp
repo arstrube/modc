c13edc0a0821c4d684e5e0cb3fcd30babcf17564
Arnd R. Strube <a.strube@wind-soft.de>
2014-10-31 07:01:05 +0100

    Chapter 7 / 3 - RomanTest.

--- c7/libraryTest/RomanTest.cpp
+++ c7/libraryTest/RomanTest.cpp
@@ -1,29 +1,31 @@
-#include "gtest/gtest.h"
+#include "CppUTest/TestHarness.h"
 #include "RomanConverter.h"
+
+TEST_GROUP(ARomanConverter) {};
 
 TEST(ARomanConverter, AnswersArabicEquivalents)
 {
    RomanConverter converter;
-   ASSERT_EQ("I", converter.convert(1));
-   ASSERT_EQ("II", converter.convert(2));
-   ASSERT_EQ("III", converter.convert(3));
-   ASSERT_EQ("IV", converter.convert(4));
-   ASSERT_EQ("V", converter.convert(5));
-   ASSERT_EQ("VI", converter.convert(6));
-   ASSERT_EQ("VII", converter.convert(7));
-   ASSERT_EQ("VIII", converter.convert(8));
-   ASSERT_EQ("IX", converter.convert(9));
-   ASSERT_EQ("X", converter.convert(10));
-   ASSERT_EQ("XIV", converter.convert(14));
-   ASSERT_EQ("XIX", converter.convert(19));
-   ASSERT_EQ("XL", converter.convert(40));
-   ASSERT_EQ("XC", converter.convert(90));
-   ASSERT_EQ("CD", converter.convert(400));
-   ASSERT_EQ("CM", converter.convert(900));
-   ASSERT_EQ("L", converter.convert(50));
-   ASSERT_EQ("C", converter.convert(100));
-   ASSERT_EQ("D", converter.convert(500));
-   ASSERT_EQ("M", converter.convert(1000));
-   ASSERT_EQ("MCMLXXXIX", converter.convert(1989));
+   STRCMP_EQUAL("I",         converter.convert(1).c_str());
+   STRCMP_EQUAL("II",        converter.convert(2).c_str());
+   STRCMP_EQUAL("III",       converter.convert(3).c_str());
+   STRCMP_EQUAL("IV",        converter.convert(4).c_str());
+   STRCMP_EQUAL("V",         converter.convert(5).c_str());
+   STRCMP_EQUAL("VI",        converter.convert(6).c_str());
+   STRCMP_EQUAL("VII",       converter.convert(7).c_str());
+   STRCMP_EQUAL("VIII",      converter.convert(8).c_str());
+   STRCMP_EQUAL("IX",        converter.convert(9).c_str());
+   STRCMP_EQUAL("X",         converter.convert(10).c_str());
+   STRCMP_EQUAL("XIV",       converter.convert(14).c_str());
+   STRCMP_EQUAL("XIX",       converter.convert(19).c_str());
+   STRCMP_EQUAL("XL",        converter.convert(40).c_str());
+   STRCMP_EQUAL("XC",        converter.convert(90).c_str());
+   STRCMP_EQUAL("CD",        converter.convert(400).c_str());
+   STRCMP_EQUAL("CM",        converter.convert(900).c_str());
+   STRCMP_EQUAL("L",         converter.convert(50).c_str());
+   STRCMP_EQUAL("C",         converter.convert(100).c_str());
+   STRCMP_EQUAL("D",         converter.convert(500).c_str());
+   STRCMP_EQUAL("M",         converter.convert(1000).c_str());
+   STRCMP_EQUAL("MCMLXXXIX", converter.convert(1989).c_str());
   // ...
 }
