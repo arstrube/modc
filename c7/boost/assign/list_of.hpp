0eb51721476839891d8723d433c355e2fa82dd20
Arnd R. Strube <a.strube@wind-soft.de>
2014-11-06 23:08:33 +0100

    Chapter 7 / 18 - finish boost::assign::list_of().

--- c7/boost/assign/list_of.hpp
+++ c7/boost/assign/list_of.hpp
@@ -1,11 +1,13 @@
+#ifndef BOOST_ASSIGN_LIST_OF_HPP
+#define BOOST_ASSIGN_LIST_OF_HPP
+
 #include <set>
 
 namespace boost
 {
-
 namespace assign_detail
 {
-    template< class T >
+    template<class T>
     class generic_list : public std::set<T>
     {
     public:
@@ -15,14 +17,12 @@ namespace assign_detail
         }
     };
 }
-
 namespace assign
 {
-    template< class T >
+    template<class T>
     inline assign_detail::generic_list<T> list_of(const T& t) {
         return assign_detail::generic_list<T>()(t);
     }
+} } // namespaces
 
-} // namespace 'assign'
-
-} // namespace 'boost'
+#endif // BOOST_ASSIGN_LIST_OF_HPP
