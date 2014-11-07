3db39da7b80284ebfbc18969566337ae106063dd
Arnd R. Strube <a.strube@wind-soft.de>
2014-11-07 12:35:07 +0100

    Wav / 6 - further simplify stubs.

--- wav/rlog/StdioNode.h
+++ wav/rlog/StdioNode.h
@@ -3,12 +3,9 @@
 namespace rlog
 {
     class RLogNode;
-
     class StdioNode {
     public:
-        StdioNode(int fileno) : fileno_(fileno){}
-        void subscribeTo(RLogNode* /*channel*/) {}
-    private:
-        int fileno_;
+        StdioNode(int) {}
+        void subscribeTo(RLogNode*) {}
     };
 } // namespace rlog
