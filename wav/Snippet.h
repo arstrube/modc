3049d307bac057003354c9b294ebda2f74494228
Arnd R. Strube <a.strube@wind-soft.de>
2014-11-13 10:30:31 +0100

    Wav / 20 - Snippet::Snippet().

--- wav/Snippet.h
+++ wav/Snippet.h
@@ -13,6 +13,16 @@ using namespace std;
 
 class Snippet {
 public:
+   Snippet(shared_ptr<FileUtil> fileUtil,
+      shared_ptr<WavDescriptor> descriptor,
+      const std::string& dest,
+      rlog::RLogChannel* channel) 
+      : fileUtil_(fileUtil)
+      , descriptor_(descriptor)
+      , dest_(dest) 
+      , channel_(channel) { }
+// ...
+
    uint32_t dataLength(
          uint32_t samples, 
          uint32_t bytesPerSample,
@@ -20,6 +30,12 @@ public:
          ) const {
       return samples * bytesPerSample * channels;
    }
+
+private:
+   shared_ptr<FileUtil> fileUtil_;
+   shared_ptr<WavDescriptor> descriptor_;
+   const string dest_;
+   rlog::RLogChannel* channel_;
 };
 
 #endif
