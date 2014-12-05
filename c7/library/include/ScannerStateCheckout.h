f6d996cac75a3748ebb2d7b32a44d31bf75b6f3e
Arnd R. Strube <a.strube@wind-soft.de>
2014-12-05 08:47:11 +0100

    Deal with -Werror,-Wunused-private-field

--- c7/library/include/ScannerStateCheckout.h
+++ c7/library/include/ScannerStateCheckout.h
@@ -1,9 +1,9 @@
 /***
  * Excerpted from "Modern C++ Programming with Test-Driven Development",
  * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
+ * Copyrights apply to this code. It may not be used to create training material,
  * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
+ * We make no guarantees that this code is fit for any purpose.
  * Visit http://www.pragmaticprogrammer.com/titles/lotdd for more book information.
 ***/
 #pragma once
@@ -25,11 +25,13 @@ public:
     virtual void ScanInventoryCard(const std::string& barcode);
     virtual void ScanPatronCard(const std::string& barcode);
     virtual void PressDone();
-    
+
     static const std::string MSG_COMPLETE_CHECKOUT_FIRST;
     static const std::string MSG_SCANNED_HOLDING;
     static const std::string MSG_INVALID_HOLDING_ID;
     static const std::string MSG_ALREADY_CHECKED_OUT;
 private:
+    #if 0 // -Werror,-Wunused-private-field
     Scanner* mScanner;
+    #endif
 };
