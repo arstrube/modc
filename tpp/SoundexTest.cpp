7cb2ed9bef0a373833e23b57b91e62cdd66883ff
Arnd R. Strube <a.strube@wind-soft.de>
2014-12-11 08:46:37 +0100

    tpp / 4

--- tpp/SoundexTest.cpp
+++ tpp/SoundexTest.cpp
@@ -12,3 +12,7 @@ TEST(SoundexEncoding, AppendsZerosToWordForOneLetterWord) {
    CHECK_EQUAL(string("A000"), soundex.encode("A"));
    CHECK_EQUAL(string("B000"), soundex.encode("B"));
 }
+
+TEST(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
+   CHECK_EQUAL(string("A100"), soundex.encode("Ab"));
+}
